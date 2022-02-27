d623d98331a16ff63a48e6b1e9b33cf42fe099be
John3331 <jonasdornbusch97@gmail.com>
2022-02-27 11:44:03 +0100

    changed raw file nameing and glob in elan annotations

--- src/preprocessing/elan_functions.py
+++ src/preprocessing/elan_functions.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 def find_corresponding_raw_frame_to_elan_annot(raw_frames_folder_path: Path, common_datum_identifier: str):
     possible_raw_frame_paths = list(
-        raw_frames_folder_path.glob(f'{common_datum_identifier}_raw.csv'))
+        raw_frames_folder_path.glob(f'**/{common_datum_identifier}*_raw.csv'))
     if len(possible_raw_frame_paths) == 0:
         raise RuntimeError(
             f'No possible raw frame found with name {common_datum_identifier}')
@@ -23,7 +23,9 @@ def add_elan_labels_to_frame(elan_annotation_folder_path: Path, raw_frames_folde
             '_annot.txt', '')
         raw_frame_file_path = find_corresponding_raw_frame_to_elan_annot(
             raw_frames_folder_path, common_datum_identifier)
-        df = pd.read_csv(raw_frame_file_path)
+
+        # sep needed, as in vs code csv extensions whitespaces are added which should not be included in the data    
+        df = pd.read_csv(raw_frame_file_path, sep=' *,', engine='python') 
 
         # set timestamp column as index, so we can do timerange based selections
         df["timestamp"] = pd.to_timedelta(df["timestamp"], unit="ms")
