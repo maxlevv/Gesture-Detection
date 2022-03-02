50d2ff5d32630b943eaff0e4acdf872804a6b477
John3331 <jonasdornbusch97@gmail.com>
2022-03-02 16:58:57 +0100

    added forearm angle feature

--- src/preprocessing/preprocessing_functions.py
+++ src/preprocessing/preprocessing_functions.py
@@ -43,13 +43,24 @@ class Labels(Enum):
 class Preprocessing_parameters():
     def __init__(self, num_shifts: int, num_timesteps: int, 
                 difference_mode: str = None, mediapipe_columns_for_diff: List[str] = None,
-                summands_pattern: List[int] = None, mediapipe_columns_for_sum: List[str] = None):
+                summands_pattern: List[int] = None, mediapipe_columns_for_sum: List[str] = None,
+                forearm_angle: bool = True):
         self.num_shifts = num_shifts
         self.num_timesteps = num_timesteps
         self.difference_mode = difference_mode
-        self.mediapipe_colums_for_diff = mediapipe_columns_for_diff
+        self.mediapipe_columns_for_diff = mediapipe_columns_for_diff
         self.summands_pattern = summands_pattern
         self.mediapipe_columns_for_sum = mediapipe_columns_for_sum
+        self.forearm_angle = forearm_angle
+    
+    def add_new_columns_to_column_lists(self, column_names: List[str]):
+        # only add to cumsum list
+ 
+        if any([column_name in self.mediapipe_columns_for_sum for column_name in column_names]):
+            return None
+
+        if not self.summands_pattern is None:
+            self.mediapipe_columns_for_sum += column_names
 
 
 def extract_features(frames: pd.DataFrame, features: list) -> pd.DataFrame:
@@ -122,10 +133,8 @@ def calc_differences(df: pd.DataFrame, preproc_params: Preprocessing_parameters)
     num_shifts = preproc_params.num_shifts
 
     # create a df with only the columns which should be considered in calculating the diff
-    df_for_diff = extract_features(df, preproc_params.mediapipe_colums_for_diff)
+    df_for_diff = extract_features(df, preproc_params.mediapipe_columns_for_diff)
 
-    # necessary as otherwise df is a view and the drop affects the original df
-    # df = df.copy()
 
     # number of samples in X
     num_samples = math.floor(
@@ -180,33 +189,70 @@ def calc_differences(df: pd.DataFrame, preproc_params: Preprocessing_parameters)
     return X, X_df
 
 
+def correct_angle_boundary_diff(diff_np: np.array):
+    # assumption: in two consecutive frames there can not be a angel diff of over np.pi in magnitude
+
+    diff_np[np.where(diff_np > np.pi)[0]] = diff_np[np.where(diff_np > np.pi)[0]] - 2 * np.pi
+    diff_np[np.where(diff_np < -np.pi)[0]] = diff_np[np.where(diff_np < -np.pi)[0]] + 2 * np.pi
+
+    return diff_np
+
+def calc_angle_diff(angle_np: np.array, window_start_index: int, num_timesteps: int):
+    angle_diff =  angle_np[window_start_index + 1: window_start_index + num_timesteps] - \
+                angle_np[window_start_index: window_start_index + num_timesteps - 1]
+    return correct_angle_boundary_diff(diff_np=angle_diff)
+
+
 def cumulative_sum(df: pd.DataFrame, preproc_params: Preprocessing_parameters) -> Tuple[np.array, pd.DataFrame]:
 
     num_timesteps = preproc_params.num_timesteps
     num_shifts = preproc_params.num_shifts
     summands_pattern = preproc_params.summands_pattern
 
+    # necessary as otherwise df is a view and the drop affects the original df
+    df = df.copy()
+
     # create a df with only the columns which should be considered in calculating the diff
     df_for_sum = extract_features(df, preproc_params.mediapipe_columns_for_sum)
-
-    # necessary as otherwise df is a view and the drop affects the original df
-    # df = df.copy()
+    
+    angle_involved = True
+    if 'right_forearm_angle' in df_for_sum.columns:
+        right_forearm_angle = df_for_sum['right_forearm_angle'].to_numpy()
+        left_forearm_angle = df_for_sum['left_forearm_angle'].to_numpy()
+        df_for_sum.drop(['right_forearm_angle', 'left_forearm_angle'], axis=1, inplace=True)
 
     # number of samples in X
     num_samples = math.floor(
         (df_for_sum.shape[0] - num_timesteps + num_shifts) / num_shifts)
 
     data = df_for_sum.to_numpy()
-    orig_columns = df_for_sum.columns
+    
+    num_angle_features = 0
 
-    num_features = data.shape[1] * sum(summands_pattern)
+    orig_columns = list(df_for_sum.columns)
+    # add the angle column names at the end
+    if angle_involved:
+        orig_columns += ['right_forearm_angle', 'left_forearm_angle']
+        num_angle_features = 2
+
+
+    num_features = (data.shape[1] + num_angle_features) * sum(summands_pattern)
 
     X = np.zeros((num_samples, num_features))
 
     for i in range(num_samples):
         window_start_index = i * num_shifts
         window_np = data[window_start_index: window_start_index + num_timesteps, :]
-        diff_window_np = window_np[1:, :] - window_np[:-1]
+        diff_window_np = window_np[1:, :] - window_np[:-1, :]
+
+        # apply scaling to diff here, as the angle should not be scaled
+        diff_window_np = scale_to_body_size_and_dist_to_camera(diff_window_np, df.loc[i * num_shifts: i * num_shifts + num_timesteps - 1, :])
+
+        if angle_involved:
+            right_forearm_angle_diff = calc_angle_diff(right_forearm_angle, window_start_index, num_timesteps)
+            left_forearm_angle_diff = calc_angle_diff(left_forearm_angle, window_start_index, num_timesteps)
+        
+            diff_window_np = np.c_[diff_window_np, right_forearm_angle_diff, left_forearm_angle_diff]
 
         # calc the cumulative sum over the whole window
         cumsum = np.cumsum(diff_window_np, axis=0)
@@ -217,8 +263,6 @@ def cumulative_sum(df: pd.DataFrame, preproc_params: Preprocessing_parameters) -
         # adding the features to X
         X[i, :] = cumsum_features.flatten('F').reshape(1, -1)
 
-        # apply scaling
-        X[i, :] = scale_to_body_size_and_dist_to_camera(X[i, :], df.loc[i * num_shifts: i * num_shifts + num_timesteps - 1, :])
 
     # creating the df
     column_cumsum_names = [orig_column +
@@ -227,6 +271,42 @@ def cumulative_sum(df: pd.DataFrame, preproc_params: Preprocessing_parameters) -
                         column_cumsum_name + f"_{str(i)}" for column_cumsum_name in column_cumsum_names for i in range(sum(summands_pattern))])
 
     return X, X_df
+
+
+def calc_forearm_angle(df: pd.DataFrame):
+    # looking at the angle values might be confusing, as the y coordinate is quasi flipped
+
+    right_wrist_x = df['right_wrist_x'].to_numpy()
+    right_wrist_y = df['right_wrist_y'].to_numpy()
+
+    right_elbow_x = df['right_elbow_x'].to_numpy()
+    right_elbow_y = df['right_elbow_y'].to_numpy()
+
+    forearm_vector_x = right_wrist_x - right_elbow_x
+    forearm_vector_y = right_wrist_y - right_elbow_y
+
+    # calc angle with only positive angles
+    angle_np = np.arctan2( forearm_vector_y, forearm_vector_x )
+    angle_np[np.where(angle_np < 0)[0]] = angle_np[np.where(angle_np < 0)[0]] + 2 * np.pi
+
+    df['right_forearm_angle'] = angle_np
+
+    left_wrist_x = df['left_wrist_x'].to_numpy()
+    left_wrist_y = df['left_wrist_y'].to_numpy()
+
+    left_elbow_x = df['left_elbow_x'].to_numpy()
+    left_elbow_y = df['left_elbow_y'].to_numpy()
+
+    forearm_vector_x = left_wrist_x - left_elbow_x
+    forearm_vector_y = left_wrist_y - left_elbow_y
+
+    # calc angle with only positive angles
+    angle_np = np.arctan2( forearm_vector_y, forearm_vector_x )
+    angle_np[np.where(angle_np < 0)[0]] = angle_np[np.where(angle_np < 0)[0]] + 2 * np.pi
+    
+    df['left_forearm_angle'] = angle_np
+
+    return df
 
 
 def determine_label_from_ground_truth_vector(ground_truth_df: pd.DataFrame, num_timesteps: int, num_shifts: int) -> np.array:
@@ -289,6 +369,14 @@ def create_X(df: pd.DataFrame, preproc_params: Preprocessing_parameters) -> Tupl
     # number of samples in X
     num_samples = math.floor(
         (df.shape[0] - preproc_params.num_timesteps + preproc_params.num_shifts) / preproc_params.num_shifts)
+
+    # note that forearm_angle is different to differences and cumsum, as is on coordinate level and needs to go through diff or cumsum 
+    if preproc_params.forearm_angle:
+        df = calc_forearm_angle(df)
+
+        # add the angle names to the lists so that diff and cumsum is also applied to it
+        preproc_params.add_new_columns_to_column_lists(
+            ['right_forearm_angle', 'left_forearm_angle'])
 
     if preproc_params.difference_mode:
         X_diff, X_diff_df = calc_differences(df, preproc_params)
@@ -361,9 +449,10 @@ if __name__ == '__main__':
     # nn_input_df.to_csv('nn_input_test.csv')
 
     preproc_params = Preprocessing_parameters(
-        num_shifts=1, num_timesteps=7, summands_pattern=[1, 1, 1, 1, 1, 1], mediapipe_columns_for_sum=mediapipe_columns_for_sum)
+        num_shifts=1, num_timesteps=7, # difference_mode='one', mediapipe_columns_for_diff= mediapipe_colums_for_diff, 
+        summands_pattern=[1, 1, 1, 1, 1, 1], mediapipe_columns_for_sum=mediapipe_columns_for_sum)
 
     handle_preprocessing(Path(r'../../data\labeled_frames\ready_to_train'), Path(
-        r'../../data\preprocessed_frames\scaled_to_torso'), preproc_params)
+        r'../../data\preprocessed_frames\with_angle'), preproc_params)
 
     print('done')
