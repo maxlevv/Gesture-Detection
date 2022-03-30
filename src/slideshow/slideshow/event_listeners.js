7f45cb827bd1586c2ec539aecd6de42f3c457564
Nina <nina.wicht@stud-mail.uni-wuerzburg.de>
2022-03-30 20:59:56 +0200

    all events working except for zoom_in and zoom_out

--- src/slideshow/slideshow/event_listeners.js
+++ src/slideshow/slideshow/event_listeners.js
@@ -1,6 +1,7 @@
 let socket = new WebSocket("ws://localhost:8000/events");
 
 socket.onmessage = function(event) {
+  const currentSlide = Reveal.getCurrentSlide();
   switch(event.data){
     case "right":
       console.log("received 'right' event");
@@ -20,25 +21,24 @@ socket.onmessage = function(event) {
       break;
     case "rotate":
       console.log("received 'rotate' event");
-      const currentSlide = Reveal.getCurrentSlide();
 
       rotateRotatables(currentSlide, 90);  // defined in helper_methods.js
       break;
     case "rotate_left":
       console.log("received 'rotate_left' event");
-      const currentSlide = Reveal.getCurrentSlide();
+//      const currentSlide = Reveal.getCurrentSlide();
 
       rotateRotatables(currentSlide, -90);  // defined in helper_methods.js
       break;
     case "rotate180":
       console.log("received 'rotate180' event");
-      const currentSlide = Reveal.getCurrentSlide();
+//      const currentSlide = Reveal.getCurrentSlide();
 
       rotateRotatables(currentSlide, 180);  // defined in helper_methods.js
       break;
     case "rotate360":
       console.log("received 'rotate360' event");
-      const currentSlide = Reveal.getCurrentSlide();
+//      const currentSlide = Reveal.getCurrentSlide();
 
       rotateRotatables(currentSlide, 360);  // defined in helper_methods.js
       break;
