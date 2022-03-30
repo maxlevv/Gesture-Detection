bb4ec5967d50be310fdbeccdb049cdb5d5243838
Nina <nina.wicht@stud-mail.uni-wuerzburg.de>
2022-03-30 21:33:39 +0200

    all events working

--- src/slideshow/slideshow/event_listeners.js
+++ src/slideshow/slideshow/event_listeners.js
@@ -21,22 +21,18 @@ socket.onmessage = function(event) {
       break;
     case "rotate":
       console.log("received 'rotate' event");
-
       rotateRotatables(currentSlide, 90);  // defined in helper_methods.js
       break;
     case "rotate_left":
       console.log("received 'rotate_left' event");
-
       rotateRotatables(currentSlide, -90);  // defined in helper_methods.js
       break;
     case "rotate180":
       console.log("received 'rotate180' event");
-
       rotateRotatables(currentSlide, 180);  // defined in helper_methods.js
       break;
     case "rotate360":
       console.log("received 'rotate360' event");
-
       rotateRotatables(currentSlide, 360);  // defined in helper_methods.js
       break;
     case "zoom_in":
