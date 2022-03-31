116a6116da01d276d3fbd202277caea6e2a4ef0e
Nina <nina.wicht@stud-mail.uni-wuerzburg.de>
2022-03-31 15:14:24 +0200

    no look net

--- src/modeling/no_look_net.py
+++ src/modeling/no_look_net.py
@@ -1,8 +1,13 @@
+import os
 from pathlib import Path
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib.ticker import MaxNLocator
 import numpy as np
 import pandas as pd
 
+from evaluation.metrics import calc_confusion_matrix
 from feature_scaling import StandardScaler
 from gradient_checking import check_gradient_of_neural_net
 from helper import softmax2one_hot
@@ -29,7 +34,6 @@ def train(X, Y_g, lr, epochs, batch_size, X_val, Y_g_val):
     my_net.clear_data_specific_parameters()
     my_net.fit(X_scaled, Y_g, lr=lr, epochs=epochs, batch_size=batch_size, optimizer='adam', X_val=X_val_scaled, Y_g_val=Y_g_val)
     my_net.clear_data_specific_parameters()
-    print(my_net.scaler.to_dict())
 
     return my_net
 
@@ -38,9 +42,51 @@ def predict(df, net):
     X = preprocess(df)
     X_scaled = net.scaler.transform(X)
     net.forward_prop(X_scaled)
-    prediction = pd.Series(softmax2one_hot(net.O[-1].T)[:, 0]).map(label_encodings_inverse)
-    print(prediction)
+    prediction = softmax2one_hot(net.O[-1].T)[:, 0][:, np.newaxis].astype(int)
     return prediction
+
+
+def plot(y_label, title, ar, dir, file_name):
+    fig, ax = plt.subplots(figsize=(10, 8))
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel(y_label)
+    ax.set_title(title)
+
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.plot(np.arange(1, len(ar) + 1, 1, dtype=int), ar)
+    # plt.show()
+    fig.savefig(dir / file_name)
+
+
+def calc_confusion_matrix_binary(h: np.array, y: np.array):
+    if np.shape(h) != np.shape(y):
+        raise Exception("ground truth vector y and hypothesis h are not the same size")
+
+    n = 2
+    matrix = np.zeros(shape=(n, n), dtype='int')
+
+    # for each row set the maximal value to one and the rest to zero
+    # instead of h.round()
+    h_onehot = softmax2one_hot(h)
+
+    for i in range(y.shape[0]):
+        matrix[h_onehot[i, 0], y[i, 0]] = matrix[h_onehot[i, 0], y[i, 0]] + 1
+
+    return matrix
+
+
+def plot_confusion_matrix_binary(confusion_matrix, dir, file_name):
+    fig, ax = plt.subplots(figsize=(10, 8))
+
+    labels = ['no_look', 'idle']
+    confusion_df = pd.DataFrame(data=confusion_matrix, columns=labels, index=labels)
+
+    sns.heatmap(confusion_df, annot=confusion_matrix , fmt="", ax=ax)
+    ax.set_xlabel("ground truth")
+    ax.set_ylabel("predicted")
+    ax.set_title("confusion matrix")
+    # plt.show()
+    fig.savefig(dir / file_name)
 
 
 label_encodings = {
@@ -63,17 +109,30 @@ df_val = pd.read_csv(
 X_val = preprocess(df_val)
 Y_g_val = df_val.loc[:, 'ground_truth'].map(label_encodings).to_numpy()[:, np.newaxis]
 
-lr = 0.01
+lr = 1
 batch_size = 64
-epochs = 2
+epochs = 10
 
 net = train(X, Y_g, lr, epochs, batch_size, X_val, Y_g_val)
-save_path = Path('/home/nina/Documents/4_Machine_Learning_for_User_Interfaces/software projects/ml_dev_repo/saved_runs/no_look/first_run_nina_no_look')
-net.save_run(save_path,
+save_path = Path('/home/nina/Documents/4_Machine_Learning_for_User_Interfaces/software projects/ml_dev_repo/saved_runs/no_look')
+
+save_folder_path = net.save_run(save_path,
              'first_run_nina_no_look', author='Nina', data_file_name='test',
              lr=lr, batch_size=batch_size, epochs=epochs, num_samples=X.shape[0],
              description="test")
 
-# load_path = Path('/home/nina/Documents/4_Machine_Learning_for_User_Interfaces/software projects/ml_dev_repo/saved_runs/no_look/first_run_nina_no_look/2022-03-30_1_1-1-1')
+# load_path = Path('/home/nina/Documents/4_Machine_Learning_for_User_Interfaces/software projects/ml_dev_repo/saved_runs/no_look/first_run_nina_no_look/first_run_nina_no_look/2022-03-31_19_1-1')
 # net = FCNN.load_run(load_path)
-predict(df, net)
+# predict(df, net)
+
+
+no_look_path = save_path / 'first_run_nina_no_look'
+for dir in next(os.walk(no_look_path))[1]:
+    dir = Path(no_look_path, dir)
+    plot(ar=net.val_acc_hist, dir=dir, y_label="Accuracy", file_name='validation_accuracy.png', title="Validation Accuracy")
+    plot(ar=net.loss_hist, dir=dir, y_label="Loss", file_name='learning_curve.png', title="Learning Curve")
+    plot(ar=net.f1_score_val_hist, dir=dir, y_label="F1 Score", file_name='f1_score.png', title="F1 Score")
+
+    h_val = predict(df_val, net)
+    confusion_matrix  = calc_confusion_matrix_binary(h_val, Y_g_val)
+    plot_confusion_matrix_binary(confusion_matrix, dir, 'Confusion_matrix.png')
