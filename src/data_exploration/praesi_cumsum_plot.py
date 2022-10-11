613dd6d08de23b94567447219272edc553bbc74f
Nina <nina-357@gmx.de>
2022-10-11 22:08:11 +0200

    improvements of praesi plot

--- src/data_exploration/praesi_cumsum_plot.py
+++ src/data_exploration/praesi_cumsum_plot.py
@@ -1,7 +1,3 @@
-import os
-import sys
-
-import numpy as np
 import pandas as pd
 from pathlib import Path
 import matplotlib.pyplot as plt
@@ -10,19 +6,11 @@ preproc_file_path = Path(r'data\preprocessed_frames\final\train\mandatory_data\0
 df = pd.read_csv(preproc_file_path, delimiter=' *,', engine='python')
 print(df.columns)
 
-# fig, axs = plt.subplots(6, 1, figsize=(80, 50))
-fig, ax = plt.subplots(figsize=(100, 100))
-#for i in range(6):
-#    axs[i].step(df.index[i*200:i*200 + 200], df.loc[i*200:i*200 + 200-1, 'right_wrist_x'], where='mid', color='blue')
-#    #axs[i].step(df.index[i*200:i*200 + 200], df.loc[i*200:i*200 + 200-1, 'right_wrist_y'], where='mid', color='green')
-#    axs[i].step(df.index[i*200:i*200 + 200], df.loc[i*200:i*200 + 200-1, 'left_wrist_x'], where='mid', color='yellow')
-#    axs[i].plot(df.index[i*200:i*200 + 200], df.loc[i*200:i*200 + 200-1, 'ground_truth'])
-ax.step(df.index[60:60 + 180], df.loc[60:60 + 180-1, 'right_wrist_x_cumsum_5'], where='mid', color='blue', label="right_wrist_x_cumsum_8")
-#ax.step(df.index[60:60 + 180], df.loc[60:60 + 180-1, 'right_wrist_y_cumsum_8'], where='mid', color='green', label="right_wrist_y_cumsum_8")
-ax.step(df.index[60:60 + 180], df.loc[60:60 + 180-1, 'right_forearm_angle_cumsum_5'], where='mid', color='green', label="right_forearm_angle_cumsum_8")
-ax.step(df.index[60:60 + 180], df.loc[60:60 + 180-1, 'right_forearm_r_cumsum_5'], where='mid', color='red', label="right_forearm_r_cumsum_8")
-#ax.step(df.index[60:60 + 180], df.loc[60:60 + 180-1, 'right_forearm_angle_cumsum_0'], where='mid', color='black', label="right_wrist_y_cumsum_8")
-ax.plot(df.index[60:60 + 180], df.loc[60:60 + 180-1, 'gt_r'])
+fig, ax = plt.subplots(figsize=(40, 17))
+ax.step(df.index[60:60 + 180], df.loc[60:60 + 180-1, 'right_wrist_x_cumsum_5'], where='mid', color='blue', label="X-coord. of right wrist")
+ax.step(df.index[60:60 + 180], df.loc[60:60 + 180-1, 'right_forearm_angle_cumsum_5'], where='mid', color='green', label="Angle of right forearm")
+ax.step(df.index[60:60 + 180], df.loc[60:60 + 180-1, 'right_forearm_r_cumsum_5'], where='mid', color='red', label="right_forearm_r")
+ax.plot(df.index[60:60 + 180], df.loc[60:60 + 180-1, 'gt_sl'], label='Ground truth (swipe left)', color='orange')
 
 ax.set_xlabel("Nummer des Frames", fontsize=30, labelpad=30)
 ax.set_ylabel("Kumulative Summe", fontsize=30, labelpad=30)
